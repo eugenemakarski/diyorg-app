89f87d2742a05ea1368ad20bd20895d102330a43
emakarsk <55766172+emakarsk@users.noreply.github.com>
2022-01-24 17:49:19 -0500

    feat - Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>DIYORG</title>
+        <meta name="keywords" content="workout plan" />
+      </Head>
+
+      <h1 className="text-3xl font-bold underline">NEXT.JS!</h1>
+    </div>
+  );
+}
